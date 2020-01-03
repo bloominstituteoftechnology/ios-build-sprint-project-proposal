20b5296146e43813cca3f8fa1258c46146ed4fa5
Gi Pyo Kim <gipyok@gmail.com>
2020-01-02 23:48:04 -0800

    able to get url from the selected video file.

--- rubato/rubato/ViewController.swift
+++ rubato/rubato/ViewController.swift
@@ -7,14 +7,124 @@
 //
 
 import UIKit
+import MobileCoreServices
+import AVFoundation
+import MediaPlayer
+import StoreKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoView: UIView!
+    
+    var videoPlayer: AVPlayer!
+    var playerLayer: AVPlayerLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func chooseVideo(_ sender: Any) {
+        presentVideoPickerController()
+//        getAuthorizationForAppleMusic()
+    }
+    
+    private func presentVideoPickerController() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary){
+            DispatchQueue.main.async {
+                let videoPicker = UIImagePickerController()
+                videoPicker.sourceType = .photoLibrary
+                videoPicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+                videoPicker.mediaTypes = [kUTTypeMovie as String]
+                videoPicker.allowsEditing = true
+                videoPicker.delegate = self
+                self.present(videoPicker, animated: true, completion: nil)
+            }
+        } else {
+            // TODO: self.presentInformationalAlertController(title: "Error", message: "The photo library or the camera is unavailable.")
+        }
+    }
+    
+    private func presentAudioPickerController() {
+        DispatchQueue.main.async {
+            let mediaPickerController = MPMediaPickerController(mediaTypes: .any)
+            mediaPickerController.delegate = self
+            mediaPickerController.prompt = "Select Audio"
+            self.present(mediaPickerController, animated: true, completion: nil)
+        }
+    }
+    
+    private func getAuthorizationForAppleMusic() {
+        let authorizationStatus:MPMediaLibraryAuthorizationStatus = MPMediaLibrary.authorizationStatus()
+        
+        switch authorizationStatus {
+        case MPMediaLibraryAuthorizationStatus.authorized:
+            presentAudioPickerController()
+            break
+        case MPMediaLibraryAuthorizationStatus.notDetermined:
+            MPMediaLibrary.requestAuthorization { (authorizationStatus) in
+                if authorizationStatus == MPMediaLibraryAuthorizationStatus.authorized {
+                    self.presentAudioPickerController()
+                } else {
+                    print("The Media Library was not authorized by the user")
+                }
+            }
+        default:
+            break
+        }
+    }
 
+    
+}
+
+
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let mediaType = info[UIImagePickerController.InfoKey.mediaType] as? String,
+            mediaType == (kUTTypeMovie as String),
+            let url = info[UIImagePickerController.InfoKey.mediaURL] as? URL else {
+                
+                // TODO: self.presentInformationalAlertController(title: "Error", message: "Please choose a video")
+                return
+        }
+        print("Video URL: \(url)")
+        
+        videoPlayer = AVPlayer(url: url)
+        
+        playerLayer = AVPlayerLayer(player: videoPlayer)
+        var topRect = view.bounds
+        topRect.size.height = topRect.height / 1.5
+        topRect.size.width = topRect.width / 1.5
+        topRect.origin.y = view.safeAreaInsets.top
+        playerLayer?.frame = topRect
+        view.layer.addSublayer(playerLayer!)
+        
+        videoPlayer.play()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController: MPMediaPickerControllerDelegate {
+    func mediaPicker(_ mediaPicker: MPMediaPickerController, didPickMediaItems mediaItemCollection: MPMediaItemCollection) {
+        mediaPicker.dismiss(animated: true) {
+            let selectedSongs = mediaItemCollection.items
+            guard let song = selectedSongs.first else { return }
+            
+            let url = song.value(forProperty: MPMediaItemPropertyAssetURL) as? URL
+            
+            print("Audio URL: \(url)")
+        }
+    }
+    
+    func mediaPickerDidCancel(_ mediaPicker: MPMediaPickerController) {
+        mediaPicker.dismiss(animated: true, completion: nil)
+    }
 }
 
