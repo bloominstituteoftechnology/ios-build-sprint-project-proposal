d8cdfae77ad47727d321676bf987effd92281b34
Gi Pyo Kim <gipyok@gmail.com>
2020-01-02 21:36:53 -0800

    slow motion code added

---
+++ rubato/rubato/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rubato
+//
+//  Created by Gi Pyo Kim on 12/30/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
