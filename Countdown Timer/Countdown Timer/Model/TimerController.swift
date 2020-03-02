e5f022075d4a8636bd9657eac897e728a3aacde3
Mark Gerrior <github@gerrior.com>
2020-03-02 14:38:47 -0800

    Mark's persistent code starter

--- Countdown Timer/Countdown Timer/Model/TimerController.swift
+++ Countdown Timer/Countdown Timer/Model/TimerController.swift
@@ -20,6 +20,48 @@ class TimerController {
                        tag: "", timer: nil)
     ]
     
+     // MARK: Persistent Store
      
-    
+     var cartURL: URL? {
+         let fileManager = FileManager.default
+         
+         let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+         
+         let cartURL = documentsDir?.appendingPathComponent("cart.plist")
+         
+         return cartURL
+     }
+     
+     func saveToPersistentStore() {
+         // Convert our shopping cart Property List
+         let encoder = PropertyListEncoder()
+         
+         do {
+             let cartData = try encoder.encode(cart)
+             
+             guard let cartURL = cartURL else { return }
+             
+             try cartData.write(to: cartURL)
+             
+         } catch {
+             print("Unable to save shopping cart to plist: \(error)")
+         }
+     }
+     
+     func loadFromPersistentStore() {
+         
+         do {
+             guard let cartURL = cartURL else { return }
+             
+             let cartData = try Data(contentsOf: cartURL)
+             
+             let decoder = PropertyListDecoder()
+             
+             let decodedCart = try decoder.decode([ShoppingItem].self, from: cartData)
+             
+             self.cart = decodedCart
+         } catch {
+             print("Unable to open shopping cart plist: \(error)")
+         }
+     }
 }
