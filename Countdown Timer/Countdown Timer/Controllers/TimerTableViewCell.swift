c6f3b64e431a30eb35ca4f33a586805a4ef2c7de
Mark Gerrior <github@gerrior.com>
2020-03-03 11:35:04 -0800

    Added TimerTableViewCell.swift

---
+++ Countdown Timer/Countdown Timer/Controllers/TimerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimerTableViewCell.swift
+//  Countdown Timer
+//
+//  Created by Mark Gerrior on 3/3/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+import UIKit
+
+class TimerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
