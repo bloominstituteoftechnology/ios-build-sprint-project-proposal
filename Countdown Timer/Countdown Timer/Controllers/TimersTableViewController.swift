478b19b6057468f0783d6c87e76e2f505a3bc8e9
Mark Gerrior <github@gerrior.com>
2020-03-05 10:54:56 -0800

    Attempt to create times in main View controller.

--- Countdown Timer/Countdown Timer/Controllers/TimersTableViewController.swift
+++ Countdown Timer/Countdown Timer/Controllers/TimersTableViewController.swift
@@ -10,20 +10,39 @@ import UIKit
 
 extension Date {
 
-    func offsetFrom(date: Date) -> String {
+    func offsetFrom(date: Date, type: TimerType) -> String {
 
         let dayHourMinuteSecond: Set<Calendar.Component> = [.day, .hour, .minute, .second]
         let difference = NSCalendar.current.dateComponents(dayHourMinuteSecond, from: date, to: self)
 
-        let seconds = "\(difference.second ?? 0)s"
-        let minutes = "\(difference.minute ?? 0)m" + " " + seconds
-        let hours = "\(difference.hour ?? 0)h" + " " + minutes
-        let days = "\(difference.day ?? 0)d" + " " + hours
+        var seconds = ""
+        var minutes = ""
+        var hours = ""
+        var days = ""
+        var typeStr = ""
 
-        if let day = difference.day, day          > 0 { return days }
-        if let hour = difference.hour, hour       > 0 { return hours }
-        if let minute = difference.minute, minute > 0 { return minutes }
-        if let second = difference.second, second > 0 { return seconds }
+        switch type {
+        case .both:
+            typeStr = " 📆⏰"
+            seconds = "\(difference.second ?? 0)s"
+            minutes = "\(difference.minute ?? 0)m" + " " + seconds
+            hours = "\(difference.hour ?? 0)h" + " " + minutes
+            days = "\(difference.day ?? 0)d" + " " + hours
+        case .date:
+            typeStr = " 📆"
+            days = "\(difference.day ?? 0) Days"
+        case .time:
+            typeStr = " ⏰"
+            seconds = "\(difference.second ?? 0)s"
+            minutes = "\(difference.minute ?? 0)m" + " " + seconds
+            hours = "\(difference.hour ?? 0)h" + " " + minutes
+            days = "\(difference.day ?? 0)d" + " " + hours
+        }
+
+        if let day = difference.day, day          > 0 { return days + typeStr }
+        if let hour = difference.hour, hour       > 0 { return hours + typeStr }
+        if let minute = difference.minute, minute > 0 { return minutes + typeStr }
+        if let second = difference.second, second > 0 { return seconds + typeStr }
         return "Done"
     }
 
@@ -168,7 +187,7 @@ class TimersTableViewController: UITableViewController /* TODO: UITableViewDataS
             
             var displayTimer = "N/A"
             if let dateTime = timer.dateTime {
-                displayTimer = dateTime.offsetFrom(date: Date())
+                displayTimer = dateTime.offsetFrom(date: Date(), type: timer.timeType)
             }
             cell.timerLabel.text = displayTimer
             row += 1
