2d75feb04c415f4aba77e89a34e897bf52b2e4ad
Mark Gerrior <github@gerrior.com>
2020-03-05 13:39:03 -0800

    timerType and timerDate for create and update func

--- Countdown Timer/Countdown Timer/Storyboards/DetailViewController.swift
+++ Countdown Timer/Countdown Timer/Storyboards/DetailViewController.swift
@@ -68,14 +68,35 @@ class DetailViewController: UIViewController {
             !event1.isEmpty {
             events.append(event1)
         }
+        
+        var timerType = TimerType.time
+        switch segmentedControl.selectedSegmentIndex{
+            case 0:
+                timerType = .both
+            case 1:
+                timerType = .time
+            case 2:
+                timerType = .date
+            default:
+                break
+            }
+
+                // .date returns the new date with the current time (GMT)
+        // I'm confused about what .time returns.
+        //  - I set time to 1h 0m
+        //  - It's currently 2020-03-05 18:45 +0000
+        //  - I got back 2020-03-05 09:00:00 +0000
+        let timerDate = datePicker.date
+        print(timerDate)
+
         if timer == nil {
             // Create timer
             // FIXME: Real values
-            timerModelDelegate?.create(emoji: emojiTextField.text ?? "", name: eventTextField?.text! ?? "", dateTime: datePicker.date, timeType: timerTypeForSegmentedControl(), active: true, tag: "")
+            timerModelDelegate?.create(emoji: emojiTextField.text ?? "", name: eventTextField?.text! ?? "", dateTime: timerDate, timeType: timerType, active: true, tag: "")
         } else {
             // Timer exists, update it
             // FIXME: Real values
-            timerModelDelegate?.udpate(timer: timer!, emoji: emojiTextField.text ?? "", name: eventTextField?.text! ?? "", dateTime: datePicker.date, timeType: timerTypeForSegmentedControl() , active: true, tag: "")
+            timerModelDelegate?.udpate(timer: timer!, emoji: emojiTextField.text ?? "", name: eventTextField?.text! ?? "", dateTime: timerDate, timeType: timerType , active: true, tag: "")
         }
         
         navigationController?.popViewController(animated: true)
