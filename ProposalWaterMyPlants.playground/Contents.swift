e27a35df83c19d83909f3ef692da36afc79a5600
Craig Belinfante <craigbasketball@aol.com>
2020-12-15 19:56:44 -0800

    added storyboard idea and sketch

--- ProposalWaterMyPlants.playground/Contents.swift
+++ ProposalWaterMyPlants.playground/Contents.swift
@@ -28,7 +28,24 @@
  3.picture/video of plant
  
  Sketch
+ see sketch document
  
+ Top Features
+ 1. Scheduling Timer
+    - this is a way for users to schedule and calendar when their plants need to be watered. The hope would be to sync their calendars with the OS of the phone.
+ 2. Push Notifications
+    - though minor, this feature is very useful to our users as reminders to water their plants
+ 3. picture/video of plant
+    - users can have this feature to keep track of their plant and its growth
+ */
+
+// MARK:- Project Plan
+/*
+Creating project board.. still need to add tasks
  
+Have not decided on specifics on the project yet. Craig to focus on av foundation. John to focus on animation. Glad to do focus on networking is about as far as we have gotten.
+
+ Organize
+ Core Data, Create Model, Create basic storyboard framework, create view controllers, create app logo, review av foundation and core image, do animations, run tests, complete app 
  */
 
