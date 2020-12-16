9bcd87655d4ee695acd73d63b2835bf164ea8ceb
Craig Belinfante <craigbasketball@aol.com>
2020-12-15 19:35:55 -0800

    brainstorming

--- ProposalWaterMyPlants.playground/Contents.swift
+++ ProposalWaterMyPlants.playground/Contents.swift
@@ -1,10 +1,34 @@
+// MARK:- Team and Application
 /*
  
  Team Members
  John, Glad, Craig
  
  App Name
+ Planty
  
+ Explain your app to someone you just met
+    This app is designed to help you water your plants on a schedule. We want to help you water healthy, happy, and growing plants!
+ 
+ Who would use your app?
+    Plant lovers and enthusiasts, but as well those special new plant owners that need help to remember watering their new swiss cheese plant from Lowe's.
+ */
+
+// MARK:- Brainstorming
+/*
+ Brainstorming
+ see document
+ 
+ Organize a list
+ see document
+ 
+ Prioritize
+ 1.scheduling timer
+ 2.push notifications
+ 3.picture/video of plant
+ 
+ Sketch
  
  
  */
+
